1f5e857420d9459114efcba46938d5eafbce80cc
unknown <you@example.com>
2019-08-15 15:24:54 +0200

    first commit*

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function timer(){
+  return (<div className = "timer">
+    <center>
+    <h1 className ="time"> 00:30:00</h1>
+    <div className=" time-m">
+    <p> hour minut second  </p>
     </div>
-  );
+    </center>
+     
+  </div>
+
+  )
 }
 
-export default App;
+         
+
+export default timer;
